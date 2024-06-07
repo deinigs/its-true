661f7a0514803b87bf7243954e2ee7d050093223
Danny Godoy <dangodoy84@gmail.com>
2024-06-06 22:35:56 -0400

    Added test. Code fix to conform to tests. Actions to run test and to deploy to npm on release create

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 /**
- * 
+ * Considered true:
+ * - true
+ * - "true" (all different letter cases)
+ * - 1
+ * - "1"
  * @param {*} value 
  * @returns {boolean} True if its considered true
  */
 function itsTrue(value) {
-    return new Boolean(value).valueOf();
+    if (typeof value == 'string' || value instanceof String) {
+        const lowerValue = value.toLowerCase();
+        return lowerValue === 'true' || lowerValue === '1';
+    }
+    if (typeof value === 'boolean') {
+        return value;
+    }
+    if (value instanceof Boolean) {
+        return value.valueOf();
+    }
+    if(typeof value === 'number') {
+        return value === 1;
+    }
+    return false;
 }
 
 module.exports = itsTrue;
